f85b304c4f5158bf6951bfa570baafa7f1ae032c
mlghr <michael.rosenbaum1@gmail.com>
2020-11-10 11:46:25 -0800

    added missing parens

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.envrion.get('DATABASE_URL', "postgres:///dnd_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.envrion.get('DATABASE_URL', "postgres:///dnd_db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY',"secret1")
